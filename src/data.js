1230ce32f617969fdfaef62e85e5fd45af4f1cdf
David Castellanos <davcastellanoslarios@gmail.com>
2023-02-08 19:02:30 -0600

    Update data.js

--- src/data.js
+++ src/data.js
@@ -4,3 +4,9 @@ export const pageLinks = [
     {id:3, href:'#services', text:'services'},
     {id:4, href:'#tours', text:'tours'},
 ]
+
+export const socialLinks = [
+    {id: 1, href:'https://www.twitter.com', icon:'fab fa-twitter'},
+    {id: 2, href:'https://www.facebook.com', icon:'fab fa-facebook'},
+    {id: 3, href:'https://www.squarespace.com', icon:'fab fa-squarespace'}
+]
