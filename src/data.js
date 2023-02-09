a9c5da59b8aad55b59e264698daa6d53166b200f
David Castellanos <davcastellanoslarios@gmail.com>
2023-02-08 18:49:36 -0600

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,6 @@
+export const pageLinks = [
+    {id:1, href:'#home', text:'home'},
+    {id:2, href:'#about', text:'about'},
+    {id:3, href:'#services', text:'services'},
+    {id:4, href:'#tours', text:'tours'},
+]
