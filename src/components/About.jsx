6296cfced3903118e9b76df02a9fdc370348d692
David Castellanos <davcastellanoslarios@gmail.com>
2023-02-08 18:18:57 -0600

    Create About.jsx

---
+++ src/components/About.jsx
@@ -0,0 +1,34 @@
+
+export const About = () => {
+  return (
+    <section className="section" id="about">
+    <div className="section-title">
+      <h2>about <span>us</span></h2>
+    </div>
+
+    <div className="section-center about-center">
+      <div className="about-img">
+        <img
+          src="./images/about.jpeg"
+          className="about-photo"
+          alt="awesome beach"
+        />
+      </div>
+      <article className="about-info">
+        <h3>explore the difference</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
+          quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
+          unde dolor?
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
+          quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
+          unde dolor?
+        </p>
+        <a href="#" className="btn">read more</a>
+      </article>
+    </div>
+  </section>
+  )
+}
