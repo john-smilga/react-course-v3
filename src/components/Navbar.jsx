770774b11040066955a813fcf944d425100235ec
David Castellanos <davcastellanoslarios@gmail.com>
2023-02-08 18:57:27 -0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import logo from '../images/logo.svg'
-
+import { pageLinks } from '../data'
+ 
 export const Navbar = () => {
     
   return (
@@ -11,23 +12,16 @@ export const Navbar = () => {
             <i className="fas fa-bars"></i>
           </button>
         </div>
+
         {/* <!-- left this comment on purpose --> */}
         <ul className="nav-links" id="nav-links">
-          <li>
-            <a href="#home" className="nav-link"> home </a>
-          </li>
-
-          <li>
-            <a href="#about" className="nav-link"> about </a>
-          </li>
-
-          <li>
-            <a href="#services" className="nav-link"> services </a>
-          </li>
-
-          <li>
-            <a href="#tours" className="nav-link"> tours</a>
-          </li>
+          {pageLinks.map((link) =>{
+            return ( 
+              <li>
+                <a href={link.href} className="nav-link"> {link.text} </a>
+              </li>
+            )
+          })}
         </ul>
 
         <ul className="nav-icons">
