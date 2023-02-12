d492317b8b3e4dd8ed8c0cafed248e711aad8cc8
David Castellanos <davcastellanoslarios@gmail.com>
2023-02-11 18:43:15 -0600

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -33,8 +33,12 @@ export const Footer = () => {
         }
       </ul>
       <p className="copyright">
+
         copyright &copy; Backroads travel tours company
-        <span id="date"></span> all rights reserved
+        <span id="date"> 
+          {new Date().getFullYear()} 
+        </span> all rights reserved.
+
       </p>
     </footer>
   )
