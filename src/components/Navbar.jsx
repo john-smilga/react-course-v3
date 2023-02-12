033e645b65f89381b15785ae4fa0703bbb955446
David Castellanos <davcastellanoslarios@gmail.com>
2023-02-11 19:12:35 -0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,26 @@
 import logo from '../images/logo.svg'
-import { socialLinks } from '../data'
 import { PageLinks } from './LinksComponetns/PageLinks'
+import { IconsLinks } from './LinksComponetns/IconsLinks'
  
 export const Navbar = () => {
     
   return (
     <nav className="navbar">
       <div className="nav-center">
+
         <div className="nav-header">
+
           <img src={logo} className="nav-logo" alt="backroads" />
           <button type="button" className="nav-toggle" id="nav-toggle">
             <i className="fas fa-bars"></i>
           </button>
+          
         </div>
 
         <PageLinks parentClass='nav-links' itemClass='nav-link'/>
 
-        <ul className="nav-icons">
-          { socialLinks.map( link => {
+        <IconsLinks parentClassIcon='nav-icons' itemClassIcon='nav-icon'/>
 
-            const { id, href, icon} = link
-
-            return (
-              <li key={id}>
-                <a href={href} target="_blank" rel='noreferrer' className="nav-icon">
-                  <i className={icon}></i>
-                </a>
-              </li>
-            )
-          })}
-        </ul>
       </div>
     </nav>
   )
