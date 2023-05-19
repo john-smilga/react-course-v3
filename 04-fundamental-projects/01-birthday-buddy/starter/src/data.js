af76e025e466f7ce0e41fe8dbf5ea84f6ed7712b
john smilga <johnsmilga@gmail.com>
2023-05-19 10:17:41 -0400

    switch birthday buddy to local images

--- 04-fundamental-projects/01-birthday-buddy/starter/src/data.js
+++ 04-fundamental-projects/01-birthday-buddy/starter/src/data.js
@@ -3,35 +3,30 @@ export default [
     id: 1,
     name: 'Bertie Yates',
     age: 29,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+    image: 'https://www.course-api.com/images/birthday-buddy/person-1.jpeg',
   },
   {
     id: 2,
     name: 'Hester Hogan',
     age: 32,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+    image: 'https://www.course-api.com/images/birthday-buddy/person-2.jpeg',
   },
   {
     id: 3,
     name: 'Larry Little',
     age: 36,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    image: 'https://www.course-api.com/images/birthday-buddy/person-3.jpeg',
   },
   {
     id: 4,
     name: 'Sean Walsh',
     age: 34,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    image: 'https://www.course-api.com/images/birthday-buddy/person-4.jpeg',
   },
   {
     id: 5,
     name: 'Lola Gardner',
     age: 29,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    image: 'https://www.course-api.com/images/birthday-buddy/person-5.jpeg',
   },
 ];
