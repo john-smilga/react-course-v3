6131b322abf9fec15b0c93494b7f0c761b79b821
Muluken Mekuriya <85693465+MulukenMekuriya@users.noreply.github.com>
2023-05-29 00:13:55 -0400

    book list increased to 10

--- 01-fundamentals/src/books.js
+++ 01-fundamentals/src/books.js
@@ -1,23 +1,65 @@
-import img1 from './images/book-1.jpg';
-import img2 from './images/book-2.jpg';
-import img3 from './images/book-3.jpg';
+import img1 from "./images/book-1.jpg";
+import img2 from "./images/book-2.jpg";
+import img3 from "./images/book-3.jpg";
 export const books = [
-  {
-    author: 'Jordan Moore',
-    title: 'Interesting Facts For Curious Minds',
-    img: img1,
-    id: 1,
-  },
-  {
-    author: 'James Clear',
-    title: 'Atomic Habits',
-    img: img2,
-    id: 2,
-  },
-  {
-    author: 'Stephen King',
-    title: 'Fairy Tale',
-    img: img3,
-    id: 3,
-  },
+	{
+		author: "Jordan Moore",
+		title: "Interesting Facts For Curious Minds",
+		img: img1,
+		id: 1,
+	},
+	{
+		author: "James Clear",
+		title: "Atomic Habits",
+		img: img2,
+		id: 2,
+	},
+	{
+		author: "Stephen King",
+		title: "Fairy Tale",
+		img: img3,
+		id: 3,
+	},
+	{
+		author: "J.K. Rowling",
+		title: "Harry Potter and the Philosopher's Stone",
+		img: img3,
+		id: 4,
+	},
+	{
+		author: "George R.R. Martin",
+		title: "A Game of Thrones",
+		img: img2,
+		id: 5,
+	},
+	{
+		author: "Jane Austen",
+		title: "Pride and Prejudice",
+		img: img1,
+		id: 6,
+	},
+	{
+		author: "Ernest Hemingway",
+		title: "The Old Man and the Sea",
+		img: img1,
+		id: 7,
+	},
+	{
+		author: "Agatha Christie",
+		title: "Murder on the Orient Express",
+		img: img3,
+		id: 8,
+	},
+	{
+		author: "Harper Lee",
+		title: "To Kill a Mockingbird",
+		img: img1,
+		id: 9,
+	},
+	{
+		author: "F. Scott Fitzgerald",
+		title: "The Great Gatsby",
+		img: img3,
+		id: 10,
+	},
 ];
